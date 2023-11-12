72a3ce4173debb0750794df3296777d739ddfd94
Me <park.pavel04@gmail.com>
2023-11-12 20:08:41 +0600

    main class

---
+++ src/src/Motorcycle.java
@@ -0,0 +1,36 @@
+public class Motorcycle {
+    private String brand;
+    private String model;
+    private String color;
+    private int maxSpeed;
+    private double price;
+
+
+    public Motorcycle(String brand, String model, String color, int maxSpeed, double price){
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
